1d0cc97af95f80dc591309bb66be884136d453d0
Alexander Delfim <alexanderdelfimjr11@gmail.com>
2023-06-06 23:05:48 -0300

    working with texts

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,20 @@ public class Main {
 
         boolean includedOnThePlan = true;
         double movieScore = 8.1;
+
+        // Average calculated from 3 ratings from 3 diferent people
+        double average = (9.8 + 6.3 + 8.0) / 3;
+
+        System.out.println("Avarege: " + average);
+
+        // String synopsis = "Adventure movie with an 80's heartthrob.";
+        String synopsis = """
+            Movie: Top Gun: Maverik
+            Launch Year: 2022
+            Avarege: 8.0
+            Synopsis: Adventure movie with an 80's heartthrob.
+                """;
+
+        System.out.println("Synopsis: " + synopsis);
     }
 }
