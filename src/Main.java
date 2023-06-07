346f595a5bdc34ddd16c56bd943aff5362889d5f
Alexander Delfim <alexanderdelfimjr11@gmail.com>
2023-06-06 22:22:51 -0300

    assigning values to the movie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Concluí a aula 01 e agora estou mergulhando em Java!");
+        System.out.println("Welcome to the Screen Match");
+        System.out.println("Movie: Top Gun: Maverik");
+
+        int launchYear = 2022;
+
+        System.out.println("Launch Year: " + launchYear);
+
+        boolean includedOnThePlan = true;
+        double movieScore = 8.1;
     }
 }
